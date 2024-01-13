89faf64d26426062611c85448ec22ea336e9162f
2kmyung <private.nugu@gmail.com>
2024-01-14 01:01:03 +0900

    Learning effect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((prev) => prev + 1);
+  console.log("I run all the time");
+  useEffect(() => {
+    console.log("awake");
+  }, []);
+
   return (
     <div className="App">
-      <h1 className={styles.title}>HelloWorld</h1>
+      <h1 className={styles.title}>{counter}</h1>
       <Button text={"Continue"}/>
+      <button onClick={onClick}>Click Me</button>
     </div>
   );
 }
