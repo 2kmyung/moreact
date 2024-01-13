d7cb1314517615b518d597455a675799f6d52dff
2kmyung <private.nugu@gmail.com>
2024-01-14 01:29:20 +0900

    Make Search Bar

--- src/App.js
+++ src/App.js
@@ -5,16 +5,31 @@ import { useState, useEffect } from "react";
 function App() {
 
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
   const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
   console.log("I run all the time");
   useEffect(() => {
     console.log("awake");
   }, []);
+  useEffect(() => {
+    if (keyword !== "") {
+      console.log("search for", keyword);
+    }
+  }, [keyword])
 
   return (
     <div className="App">
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here"
+      />
       <h1 className={styles.title}>{counter}</h1>
-      <Button text={"Continue"}/>
+      <Button text={"Continue"} />
       <button onClick={onClick}>Click Me</button>
     </div>
   );
