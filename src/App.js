423ccd40bb9e26401b361999a57cfc8db1279d3b
2kmyung <private.nugu@gmail.com>
2024-01-14 18:19:35 +0900

    Learning Cleanup

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
+function Hello() {
+
+  function onCreated() {
+    console.log("hi");
+    return onDestroyed;
+  }
+
+  function onDestroyed() {
+    console.log("bye");
+  }
+
+  useEffect(onCreated, [])
+
+  return <h1>Hello</h1>
+}
+
 function App() {
 
   const [counter, setCounter] = useState(0);
   const [keyword, setKeyword] = useState("");
+  const [showing, setShowing] = useState(false);
 
   const onClick = () => setCounter((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
+
 
   console.log("I run all the time");
   useEffect(() => {
@@ -22,6 +40,7 @@ function App() {
 
   return (
     <div className="App">
+      {showing ? <Hello /> : null}
       <input
         value={keyword}
         onChange={onChange}
@@ -31,6 +50,7 @@ function App() {
       <h1 className={styles.title}>{counter}</h1>
       <Button text={"Continue"} />
       <button onClick={onClick}>Click Me</button>
+      <button onClick={() => setShowing((prev) => !prev)}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
