e584979cb3dab7d2107a87e9f8c277f32f15f273
2kmyung <private.nugu@gmail.com>
2024-01-20 01:24:51 +0900

    Coin Tracker -ing

--- src/App.js
+++ src/App.js
@@ -1,40 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
 
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [selection, setSelection] = useState({});
 
-  const onChange = (event) => { setToDo(event.target.value); }
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-
-    setToDos((current) => [toDo, ...current]);
-    setToDo("");
-
-    console.log(toDo);
-  };
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      })
+  }, []);
 
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write To Do"
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => <li key={index}>{item}</li>)}
-      </ul>
-    </div>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <select
+            onChange={(event) => (console.log(event.target.value))}
+          >
+            {coins.map((coin) => (
+              <option value={coin}>
+                {coin.name} ({coin.symbol})
+              </option>
+            ))}
+          </select>
+          <input
+            type="text"
+            placeholder="ShutUpAndGiveMeMoney"
+            onChange={(event) => (setMoney(event.target.value))}
+            value={money}
+          />
+          <h2>{money} USD for 10({selection === null ? "a" : selection.name})</h2>
+        </div>
+      )
+      }
+
+    </div >
   )
 };
 
